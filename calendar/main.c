8571a6efaff4f113600b30e7b38655d929fa4477
Patrick Günthard <patrick@patlt002.fritz.box>
2017-12-27 21:14:55 +0100

    Initial Commit

---
+++ calendar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calendar
+//
+//  Created by Patrick Günthard on 27.12.17.
+//  Copyright © 2017 Patrick Günthard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
